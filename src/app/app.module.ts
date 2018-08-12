4044d59855afab3c78cba3fa8bd7c67e4c76e39a
unknown <you@example.com>
2018-08-12 18:48:23 +0200

    initial commit master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { LogoutComponent } from './auth/logout/logout.component';
+import { ForgotComponent } from './auth/forgot/forgot.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AccountComponent } from './pages/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    HomeComponent,
+    AuthComponent,
+    LoginComponent,
+    LogoutComponent,
+    ForgotComponent,
+    RegisterComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
